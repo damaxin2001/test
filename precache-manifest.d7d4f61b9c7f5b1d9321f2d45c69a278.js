2739c45394e96b684497f9521de9d678a588593b
damaxin2001 <59181931+damaxin2001@users.noreply.github.com>
2022-02-02 21:27:23 +0000

    Updates

---
+++ precache-manifest.d7d4f61b9c7f5b1d9321f2d45c69a278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfdae717d64551d86dbc",
+    "url": "/test/static/css/main.2660b4d1.chunk.css"
+  },
+  {
+    "revision": "bfdae717d64551d86dbc",
+    "url": "/test/static/js/main.bfdae717.chunk.js"
+  },
+  {
+    "revision": "884a146e5f5f265095b8",
+    "url": "/test/static/css/1.a3617c03.chunk.css"
+  },
+  {
+    "revision": "884a146e5f5f265095b8",
+    "url": "/test/static/js/1.884a146e.chunk.js"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "df91ac3000eb735fe806f5dcdd49fa95",
+    "url": "/test/index.html"
+  }
+];
